fb57399bcab3b359284c7d77331663fa26475bba
Andrew <santechserp@gmail.com>
2018-07-28 01:46:12 +0300

    доработка ошибка в подключение

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 require_once 'sett_conn.php'; // подкл скрип с данными БД
 
 // подключаемся к серверу
-$link = mysqli_connect($host, $user, $pass, $database) 
+$link = mysqli_connect($host, $user, $password, $database) 
     or die("Ошибка " . mysqli_error($link));
 
 
